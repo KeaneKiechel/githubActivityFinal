85d2043bc8d22b367bfa33a6f383862ae598f2d6
Keane Kiechel B Tumanda <keanekiechel.tumanda@g.msuiit.edu.ph>
2022-04-28 11:29:58 +0800

    Added final and print command

--- src/githubActivityFinal/Main.java
+++ src/githubActivityFinal/Main.java
@@ -6,6 +6,15 @@ public class Main {
 		 
 		Teacher t1 = new Teacher();
 		Student s1 = new Student();
+		
+		s1.name = "Al-Raffy Sarip";
+		s1.idNumber = "2020-0509";
+		
+		t1.name = "Mohammad Yassin L. Macarambon";
+		t1.idNumber = "2020-4023";
+		
+		System.out.println("Student's Name: " + s1.idNumber + " " + s1.name);
+		System.out.println("Teacher's Name: " + t1.idNumber + " " + t1.name);
 
 	}
 
